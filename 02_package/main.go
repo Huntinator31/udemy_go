cc2380a2b9675052755e88269de974bfd2fff973
Huntinator31 <khanson2012@my.fit.edu>
2016-02-05 14:35:14 -0500

    package lesson

---
+++ 02_package/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Huninator31/udemy_go/02_package/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG, olleH"))
+	fmt.Println(stringutil.MyName)
+}
